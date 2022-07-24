35c0dbdabc0ab8b19c591711a204091548a0ace5
yunyeojun <yyj8771@naver.com>
2022-07-24 21:39:39 +0900

    크롤러 중복 처리 기능 버그 수정

--- src/main/java/inha/crawler/service/notices/NoticesService.java
+++ src/main/java/inha/crawler/service/notices/NoticesService.java
@@ -31,39 +31,95 @@ public class NoticesService {
         for(CategoriesListResponseDto categoriesListResponseDto : categoriesList){
             final String url = categoriesListResponseDto.getUrl();
             Connection conn = Jsoup.connect(url);
-            try {
-                Document document = conn.get();
 
-                List<String> items = document.getElementsByAttributeValue("class", "artclTable artclHorNum1").select("strong").eachText();
-                List<String> dates = document.select("._artclTdRdate").eachText();
-                List<String> links = document.getElementsByAttributeValue("class", "artclTable artclHorNum1").select("a").eachAttr("href");
-
-                for(int i = 0; i < items.size(); i++) {
-                    String[] yearMonthDay = dates.get(i).split("\\.");
-
-                    int year = Integer.parseInt(yearMonthDay[0]);
-                    int month = Integer.parseInt(yearMonthDay[1]);
-                    int day = Integer.parseInt(yearMonthDay[2]);
-
-                    List<NoticesListResponseDto> duplicationCheck = noticesRepository.duplicationCheck(items.get(i), categoriesListResponseDto.getPage(), categoriesListResponseDto.getTag());
-                    System.out.println(duplicationCheck.size());
-                    if (duplicationCheck.size() == 0) {
-                        NoticesSaveRequestDto noticesSaveRequestDto = NoticesSaveRequestDto.builder()
-                                .title(items.get(i))
-                                .url(categoriesListResponseDto.getNoticeUrl() + links.get(i))
-                                .page(categoriesListResponseDto.getPage())
-                                .tag(categoriesListResponseDto.getTag())
-                                .year(year)
-                                .month(month)
-                                .day(day)
-                                .build();
-
-                        noticesRepository.save(noticesSaveRequestDto.toEntity());
-                    }
-                }
-            } catch (IOException e1) {
-                System.out.println("getNoticesList()에서 오류가 발생했습니다.");
+            if (url.equals("https://www.inha.ac.kr/bbs/kr/8/artclList.do")) {
+                updateNoticesList_School(url, categoriesListResponseDto.getPage(), categoriesListResponseDto.getTag(), categoriesListResponseDto.getNoticeUrl());
             }
+            else {
+                updateNoticesList_department(url, categoriesListResponseDto.getPage(), categoriesListResponseDto.getTag(), categoriesListResponseDto.getNoticeUrl());
+            }
+
+        }
+    }
+
+
+    @Transactional // 학교 홈페이지 크롤링
+    public void updateNoticesList_School(String url, String page, String tag, String noticeUrl){
+        Connection conn = Jsoup.connect(url);
+        String attributeValue = "artclLinkView";
+
+        try {
+            Document document = conn.get();
+
+            List<String> items = document.getElementsByAttributeValue("class", attributeValue).eachText();
+            List<String> dates = document.select("._artclTdRdate").eachText();
+            List<String> links = document.getElementsByAttributeValue("class", attributeValue).select("a").eachAttr("href");
+
+
+            for(int i = 0; i < items.size(); i++) {
+                String[] yearMonthDay = dates.get(i).split("\\.");
+
+                int year = Integer.parseInt(yearMonthDay[0]);
+                int month = Integer.parseInt(yearMonthDay[1]);
+                int day = Integer.parseInt(yearMonthDay[2]);
+
+                List<NoticesListResponseDto> duplicationCheck = noticesRepository.duplicationCheck(items.get(i), page, tag);
+
+                if (duplicationCheck.size() == 0) {
+                    NoticesSaveRequestDto noticesSaveRequestDto = NoticesSaveRequestDto.builder()
+                            .title(items.get(i))
+                            .url(noticeUrl + links.get(i))
+                            .page(page)
+                            .tag(tag)
+                            .year(year)
+                            .month(month)
+                            .day(day)
+                            .build();
+                    noticesRepository.save(noticesSaveRequestDto.toEntity());
+                }
+            }
+        } catch (IOException e1) {
+            System.out.println("getNoticesList()에서 오류가 발생했습니다.");
+        }
+    }
+
+    @Transactional //학과 홈페이지 크롤링
+    public void updateNoticesList_department(String url, String page, String tag, String noticeUrl){
+        Connection conn = Jsoup.connect(url);
+        String attributeValue = "artclTable artclHorNum1";
+
+        try {
+            Document document = conn.get();
+
+            List<String> items = document.getElementsByAttributeValue("class", attributeValue).select("strong").eachText();
+            List<String> dates = document.select("._artclTdRdate").eachText();
+            List<String> links = document.getElementsByAttributeValue("class", attributeValue).select("a").eachAttr("href");
+
+
+            for(int i = 0; i < items.size(); i++) {
+                String[] yearMonthDay = dates.get(i).split("\\.");
+
+                int year = Integer.parseInt(yearMonthDay[0]);
+                int month = Integer.parseInt(yearMonthDay[1]);
+                int day = Integer.parseInt(yearMonthDay[2]);
+
+                List<NoticesListResponseDto> duplicationCheck = noticesRepository.duplicationCheck(items.get(i), page, tag);
+
+                if (duplicationCheck.size() == 0) {
+                    NoticesSaveRequestDto noticesSaveRequestDto = NoticesSaveRequestDto.builder()
+                            .title(items.get(i))
+                            .url(noticeUrl + links.get(i))
+                            .page(page)
+                            .tag(tag)
+                            .year(year)
+                            .month(month)
+                            .day(day)
+                            .build();
+                    noticesRepository.save(noticesSaveRequestDto.toEntity());
+                }
+            }
+        } catch (IOException e1) {
+            System.out.println("getNoticesList()에서 오류가 발생했습니다.");
         }
     }
 
