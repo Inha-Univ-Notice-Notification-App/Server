83ecb1e0eaddee8917d4ad5bebd89691dec79a51
yunyeojun <yyj8771@naver.com>
2022-08-15 16:01:55 +0900

    swagger api 문서 기능 추가

---
+++ src/main/java/inha/crawler/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package inha.crawler.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("오늘의 인하")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("오늘의 인하")
+                .description("오늘의 인하 서버 API 문서")
+                .build();
+    }
+}
